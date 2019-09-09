c8d53347bc16036c39f58553e5c5a0c32e755e1d
Adamantios <manoszaras94@gmail.com>
2019-09-09 18:31:18 +0300

    Initial commit

---
+++ train_network.py
@@ -0,0 +1,147 @@
+import os
+import pickle
+from typing import Union
+
+from keras_preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras import Sequential
+from tensorflow.python.keras.callbacks import ModelCheckpoint
+from tensorflow.python.keras.datasets import cifar10
+from tensorflow.python.keras.optimizers import rmsprop, adam, adamax, adadelta, adagrad, sgd
+from tensorflow.python.keras.utils import to_categorical
+
+from networks.custom_network import custom_network
+from utils import create_training_parser, create_path
+
+
+def preprocess_images(images_array):
+    """
+    Preprocess the given images.
+
+    :param images_array: array containing the images to be preprocessed.
+    :return: the preprocessed images array.
+    """
+    return images_array / 255
+
+
+def create_model() -> Sequential:
+    if start_point != '':
+        if os.path.isfile(start_point):
+            return custom_network(input_shape=x_train.shape[1:], weights_path=start_point, n_classes=n_classes)
+        else:
+            raise FileNotFoundError('Checkpoint file \'{}\' not found.'.format(start_point))
+    else:
+        return custom_network(input_shape=x_train.shape[1:], n_classes=n_classes)
+
+
+def initialize_optimizer() -> Union[adam, rmsprop, sgd, adagrad, adadelta, adamax]:
+    """
+    Initializes an optimizer based on the user's choices.
+
+    :return: the optimizer.
+    """
+    if optimizer_name == 'adam':
+        return adam(lr=learning_rate, beta_1=beta1, beta_2=beta2, decay=decay)
+    elif optimizer_name == 'rmsprop':
+        return rmsprop(lr=learning_rate, rho=rho, decay=decay)
+    elif optimizer_name == 'sgd':
+        return sgd(lr=learning_rate, momentum=momentum, decay=decay)
+    elif optimizer_name == 'adagrad':
+        return adagrad(lr=learning_rate, decay=decay)
+    elif optimizer_name == 'adadelta':
+        return adadelta(lr=learning_rate, rho=rho, decay=decay)
+    elif optimizer_name == 'adamax':
+        return adamax(lr=learning_rate, beta_1=beta1, beta_2=beta2, decay=decay)
+    else:
+        raise ValueError('An unexpected optimizer name has been encountered.')
+
+
+def init_callbacks() -> []:
+    callbacks = []
+    if not omit_checkpoint:
+        # Create path for the file.
+        create_path(checkpoint_filepath)
+
+        # Create checkpoint.
+        checkpoint = ModelCheckpoint(checkpoint_filepath, monitor='val_acc', verbose=verbosity,
+                                     save_best_only=True, mode='max')
+        callbacks.append(checkpoint)
+
+    return callbacks
+
+
+def save_results() -> None:
+    # Save weights.
+    if not omit_weights:
+        # Create path for the file.
+        create_path(weights_filepath)
+        # Save weights.
+        model.save_weights(weights_filepath)
+        print('Network\'s weights have been saved as {}.\n'.format(weights_filepath))
+
+    # Save history.
+    if not omit_history:
+        # Create path for the file.
+        create_path(weights_filepath)
+        # Save history.
+        with open(hist_filepath, 'wb') as file:
+            pickle.dump(history.history, file)
+        print('Network\'s history has been saved as {}.\n'.format(hist_filepath))
+
+
+if __name__ == '__main__':
+    # Get arguments.
+    args = create_training_parser().parse_args()
+    start_point = args.start_point
+    omit_weights = args.omit_weights
+    omit_checkpoint = args.omit_checkpoint
+    omit_history = args.omit_history
+    weights_filepath = args.weights_filepath
+    hist_filepath = args.history_filepath
+    checkpoint_filepath = args.checkpoint_filepath
+    optimizer_name = args.optimizer
+    learning_rate = args.learning_rate
+    clip_norm = args.clip_norm
+    clip_value = args.clip_value
+    beta1 = args.beta1
+    beta2 = args.beta2
+    rho = args.rho
+    momentum = args.momentum
+    decay = args.decay
+    batch_size = args.batch_size
+    evaluation_batch_size = args.evaluation_batch_size
+    epochs = args.epochs
+    verbosity = args.verbosity
+
+    # Load dataset.
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    n_classes = 10
+
+    # Preprocess data.
+    x_train = preprocess_images(x_train.copy())
+    x_test = preprocess_images(x_test.copy())
+    y_train = to_categorical(y_train, 10)
+    y_test = to_categorical(y_test, 10)
+
+    # Generate batches of tensor image data with real-time data augmentation.
+    datagen = ImageDataGenerator(rotation_range=10, zoom_range=0.1, width_shift_range=0.1, height_shift_range=0.1)
+    datagen.fit(x_train)
+
+    # Create model.
+    model = create_model()
+
+    # Initialize optimizer.
+    optimizer = initialize_optimizer()
+
+    # Compile model.
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+    # Initialize callbacks list.
+    callbacks_list = init_callbacks()
+
+    # Fit network.
+    history = model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size), epochs=epochs,
+                                  steps_per_epoch=x_train.shape[0] // batch_size, validation_data=(x_test, y_test),
+                                  callbacks=callbacks_list)
+
+    # Save results.
+    save_results()
---
+++ utils.py
@@ -0,0 +1,100 @@
+from argparse import ArgumentParser
+from os import path, makedirs
+
+# ----------------------------------- DEFAULT ARGUMENTS ------------------------------------------
+
+START_POINT = ''
+SAVE_WEIGHTS = True
+SAVE_CHECKPOINT = True
+SAVE_HIST = True
+WEIGHTS_FILENAME = 'out/network_weights.h5'
+CHECKPOINT_FILENAME = 'out/checkpoint.h5'
+HIST_FILENAME = 'out/train_history.pickle'
+OPTIMIZER = 'rmsprop'
+OPTIMIZER_CHOICES = 'adam', 'rmsprop', 'sgd', 'adagrad', 'adadelta', 'adamax'
+LEARNING_RATE = 1E-3
+CLIP_NORM = 1
+CLIP_VALUE = .5
+BETA1 = .9
+BETA2 = .999
+RHO = .9
+MOMENTUM = .0
+DECAY = 1E-6
+TRAIN_BATCH_SIZE = 64
+EVAL_BATCH_SIZE = 128
+EPOCHS = 125
+VERBOSITY = 1
+
+
+# ------------------------------------------------------------------------------------------------
+
+
+def create_training_parser() -> ArgumentParser:
+    """
+    Creates an argument parser for the network training script.
+
+    :return: ArgumentParser object.
+    """
+    parser = ArgumentParser(description='Training the custom CNN network, to be used for style transferring.',
+                            epilog='Note: '
+                                   'The hyperparameters will be ignored if the chosen optimizer does not use them.')
+    parser.add_argument('-sp', '--start_point', type=str, required=False, default=START_POINT,
+                        help='Filepath containing existing weights to initialize the model.')
+    parser.add_argument('-ow', '--omit_weights', default=not SAVE_WEIGHTS, required=False, action='store_true',
+                        help='Whether the weights should not be saved (default %(default)s).')
+    parser.add_argument('-oc', '--omit_checkpoint', default=not SAVE_CHECKPOINT, required=False, action='store_true',
+                        help='Whether the best weights checkpoint should not be saved (default %(default)s).')
+    parser.add_argument('-oh', '--omit_history', default=not SAVE_HIST, required=False, action='store_true',
+                        help='Whether the training history should not be saved (default %(default)s).')
+    parser.add_argument('-wf', '--weights_filepath', default=WEIGHTS_FILENAME, required=False, type=str,
+                        help='Path to store the trained network\'s weights (default %(default)s). '
+                             'Ignored if --omit_weights has been chosen')
+    parser.add_argument('-hf', '--history_filepath', default=HIST_FILENAME, required=False, type=str,
+                        help='Path to store the trained network\'s history (default %(default)s). '
+                             'Ignored if --omit_history has been chosen')
+    parser.add_argument('-cf', '--checkpoint_filepath', default=CHECKPOINT_FILENAME, required=False, type=str,
+                        help='Path to store the trained network\'s best checkpoint(default %(default)s). '
+                             'Ignored if --omit_checkpoint has been chosen')
+    parser.add_argument('-o', '--optimizer', type=str.lower, default=OPTIMIZER, required=False,
+                        choices=OPTIMIZER_CHOICES,
+                        help='The optimizer to be used. (default %(default)s).')
+    parser.add_argument('-lr', '--learning_rate', type=float, default=LEARNING_RATE, required=False,
+                        help='The learning rate for the optimizer (default %(default)s).')
+    parser.add_argument('-cn', '--clip_norm', type=float, default=CLIP_NORM, required=False,
+                        help='The clip norm for the optimizer (default %(default)s).')
+    parser.add_argument('-cv', '--clip_value', type=float, default=CLIP_VALUE, required=False,
+                        help='The clip value for the optimizer (default %(default)s).')
+    parser.add_argument('-b1', '--beta1', type=float, default=BETA1, required=False,
+                        help='The beta 1 for the optimizer (default %(default)s).')
+    parser.add_argument('-b2', '--beta2', type=float, default=BETA2, required=False,
+                        help='The beta 2 for the optimizer (default %(default)s).')
+    parser.add_argument('-rho', type=float, default=RHO, required=False,
+                        help='The rho for the optimizer (default %(default)s).')
+    parser.add_argument('-m', '--momentum', type=float, default=MOMENTUM, required=False,
+                        help='The momentum for the optimizer (default %(default)s).')
+    parser.add_argument('-d', '--decay', type=float, default=DECAY, required=False,
+                        help='The decay for the optimizer (default %(default)s).')
+    parser.add_argument('-bs', '--batch_size', type=int, default=TRAIN_BATCH_SIZE, required=False,
+                        help='The batch size for the optimization (default %(default)s).')
+    parser.add_argument('-ebs', '--evaluation_batch_size', type=int, default=EVAL_BATCH_SIZE, required=False,
+                        help='The batch size for the evaluation (default %(default)s).')
+    parser.add_argument('-e', '--epochs', type=int, default=EPOCHS, required=False,
+                        help='The number of epochs to train the network (default %(default)s).')
+    parser.add_argument('-v', '--verbosity', type=int, default=VERBOSITY, required=False,
+                        help='The verbosity for the optimization procedure (default %(default)s).')
+
+    return parser
+
+
+def create_path(filepath: str) -> None:
+    """
+    Creates a path to a file, if it does not exist.
+
+    :param filepath: the filepath.
+    """
+    # Get the file's directory.
+    directory = path.dirname(filepath)
+
+    # Create directory if it does not exist
+    if not path.exists(directory):
+        makedirs(directory)
